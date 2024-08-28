fe314064de94fb168d5082294144bfdb3256f0e0
salmanm92 <salman@trailfive.com>
2024-08-28 18:30:53 +0500

    first

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
+const path = require('path');
 var app = express();
 
 app.use(cors());
@@ -11,6 +12,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 
